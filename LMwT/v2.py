7bef9dfee460f868ddcd9ac271d64f4b71541274
Lucas <lucasb>
2026-01-05 19:42:03 -0300

    remove unused import

--- LMwT/v2.py
+++ LMwT/v2.py
@@ -3,7 +3,7 @@
 
 import torch
 import torch.nn as nn
-from torch.nn import Dropout, functional as F
+from torch.nn import functional as F
 
 # ###########################################
 # Hyperparams
