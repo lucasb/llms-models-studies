f0124d996b27a5f31464fc2621513668dd16dda7
Lucas <lucasboscaini@gmail.com>
2026-01-26 17:47:25 -0300

    wip: tokanaizer class struct

--- tokenization/tokenizer.py
+++ tokenization/tokenizer.py
@@ -1,2 +1,18 @@
 class Tokenizer:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def train(self, text: str, vocab_size: int, verbose: bool = False) -> None:
+        pass
+
+    def encode(self, text: str) -> list[int]:
+        return []
+
+    def decode(self, ids: list[int]) -> str:
+        return ""
+
+    def save(self, model_name: str) -> None:
+        pass
+
+    def load(self, model_file: str) -> None:
+        pass
