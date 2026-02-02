e9eb43c34500b445ad2db1475e26ee01a7aabb42
Lucas <lucasboscaini@gmail.com>
2026-02-02 16:37:59 -0300

    implement build vocab func as standard 256 bytes

--- tokenization/tokenizer.py
+++ tokenization/tokenizer.py
@@ -1,6 +1,19 @@
 class Tokenizer:
     def __init__(self) -> None:
-        pass
+        self.merges = {}
+        self.spacials = {}
+        self.vocab = {}
+
+    def _build_vocab(self) -> dict:
+        # base vocab with only chars
+        vocab = {i: bytes([i]) for i in range(256)}
+        # merge chars vocab to reduce context length needed
+        for (idx1, idx2), i in self.merges.items():
+            vocab[i] = vocab[idx1] + vocab[idx2]
+        # sequence of chars to represent special behavor tokens
+        for spacial_token, i in self.spacials.items():
+            vocab[i] = spacial_token.encode("utf-8")
+        return vocab
 
     def train(self, text: str, vocab_size: int, verbose: bool = False) -> None:
         pass
