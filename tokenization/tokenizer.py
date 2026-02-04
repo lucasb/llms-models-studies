fa7f6b20fcb4a7ef4b00f22d106e5fe61cc09312
Lucas <lucasb>
2026-02-04 00:20:14 -0300

    add txt file to experiment and implement basic byte pair functions

--- tokenization/tokenizer.py
+++ tokenization/tokenizer.py
@@ -1,3 +1,33 @@
+# get pair from a list of integer
+# ex. [1,2,3,1,2] -> {(1,2): 2. (2,3): 1, (3,1): 1}
+def get_pair_frequency_counts(
+    vocab_ids: list[int], counts_to_update: dict = {}
+) -> dict:
+    counts = counts_to_update
+    vocab_ids_length = len(vocab_ids)
+    for i in range(vocab_ids_length):
+        if i < vocab_ids_length - 1:
+            # get current and next id to build pair
+            pair = (vocab_ids[i], vocab_ids[i + 1])
+            counts[pair] = counts.get(pair, 0) + 1
+    return counts
+
+
+# replace all pair occurency with new single id
+# ex. [1,2,3,1,2], (1,2), 4 -> [4,3,4]
+def merge_pair(vocab_ids: list[int], pair: tuple[int, int], pair_id: int) -> list[int]:
+    merged_ids = []
+    i = 0
+    while i < len(vocab_ids):
+        if vocab_ids[i] == pair[0] and vocab_ids[i + 1] == pair[1]:
+            merged_ids.append(pair_id)
+            i = i + 2
+        else:
+            merged_ids.append(vocab_ids[i])
+            i = i + 1
+    return merged_ids
+
+
 class Tokenizer:
     def __init__(self) -> None:
         self.merges = {}
@@ -29,3 +59,11 @@ class Tokenizer:
 
     def load(self, model_file: str) -> None:
         pass
+
+
+if __name__ == "__main__":
+    file = "wikipedia_tartan.txt"
+    with open(file, "r", encoding="utf-8") as f:
+        content = f.read()
+        text_bytes = content.encode("utf-8")  # convert to bytes
+        print(max(text_bytes))
