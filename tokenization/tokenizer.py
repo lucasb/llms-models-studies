614d2d0d58855efcfce26dc84cca672daeb1e78a
Lucas <lucasb>
2026-02-05 01:08:34 -0300

    implement the bbasic version

--- tokenization/tokenizer.py
+++ tokenization/tokenizer.py
@@ -1,9 +1,9 @@
 # get pair from a list of integer
 # ex. [1,2,3,1,2] -> {(1,2): 2. (2,3): 1, (3,1): 1}
 def get_pair_frequency_counts(
-    vocab_ids: list[int], counts_to_update: dict = {}
+    vocab_ids: list[int], counts_to_update: dict = None
 ) -> dict:
-    counts = counts_to_update
+    counts = {} if counts_to_update is None else counts_to_update
     vocab_ids_length = len(vocab_ids)
     for i in range(vocab_ids_length):
         if i < vocab_ids_length - 1:
@@ -19,7 +19,11 @@ def merge_pair(vocab_ids: list[int], pair: tuple[int, int], pair_id: int) -> lis
     merged_ids = []
     i = 0
     while i < len(vocab_ids):
-        if vocab_ids[i] == pair[0] and vocab_ids[i + 1] == pair[1]:
+        if (
+            i < len(vocab_ids) - 1
+            and vocab_ids[i] == pair[0]
+            and vocab_ids[i + 1] == pair[1]
+        ):
             merged_ids.append(pair_id)
             i = i + 2
         else:
@@ -46,13 +50,55 @@ class Tokenizer:
         return vocab
 
     def train(self, text: str, vocab_size: int, verbose: bool = False) -> None:
-        pass
+        assert vocab_size >= 256
+        num_merges = vocab_size - 256
+
+        # vocab ids from training text
+        text_bytes = text.encode("utf-8")
+        text_vocab_ids = list(text_bytes)
+
+        vocab = self._build_vocab()
+        merges = {}  # pairs(int, int), pair_id(int)
+
+        for i in range(num_merges):
+            pair_frequency = get_pair_frequency_counts(text_vocab_ids)
+
+            # more frequenty pair
+            # key need to look to value max insted of key max
+            pair = max(pair_frequency, key=pair_frequency.get)
+            vocab_pair_id = len(vocab)
+
+            # add on merge list and vocab
+            merges[pair] = vocab_pair_id
+            # save the concatened pair bytes in the new index
+            vocab[vocab_pair_id] = vocab[pair[0]] + vocab[pair[1]]
+
+            # update text_vocab_ids from training text
+            text_vocab_ids = merge_pair(text_vocab_ids, pair, vocab_pair_id)
+
+            if verbose:
+                print(
+                    f"merge {i + 1}/{num_merges}: {pair} -> {vocab_pair_id} ({vocab[vocab_pair_id]}) had {pair_frequency[pair]} occurences"
+                )
+
+        self.merges = merges  # to use in encode()
+        self.vocab = vocab  # to us in decode()
 
     def encode(self, text: str) -> list[int]:
-        return []
+        text_bytes = text.encode("utf-8")
+        vocab_ids = list(text_bytes)
 
-    def decode(self, ids: list[int]) -> str:
-        return ""
+        if len(vocab_ids) > 1:
+            # local copy to iterate
+            merges_sorted = dict(sorted(self.merges.items(), key=lambda item: item[1]))
+            for pair, pair_id in merges_sorted.items():
+                vocab_ids = merge_pair(vocab_ids, pair, pair_id)
+
+        return vocab_ids
+
+    def decode(self, vocab_ids: list[int]) -> str:
+        text_bytes = b"".join(self.vocab[vocab_id] for vocab_id in vocab_ids)
+        return text_bytes.decode("utf-8", errors="replace")
 
     def save(self, model_name: str) -> None:
         pass
@@ -62,8 +108,16 @@ class Tokenizer:
 
 
 if __name__ == "__main__":
+    text = "aaaabbaaacccc"
+    tokenizer = Tokenizer()
+
     file = "wikipedia_tartan.txt"
     with open(file, "r", encoding="utf-8") as f:
         content = f.read()
-        text_bytes = content.encode("utf-8")  # convert to bytes
-        print(max(text_bytes))
+        tokenizer.train(content, 1000, True)
+
+    print("----")
+    print(tokenizer.merges)
+    enc = tokenizer.encode(text)
+    dec = tokenizer.decode(enc)
+    print(text, enc, dec)
