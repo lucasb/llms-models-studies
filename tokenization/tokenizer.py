96f60dbbd7f47975885e2ba28b843791ad448fbe
Lucas <lucasb>
2026-02-09 23:46:36 -0300

    fix the load to complile the regex pattern

--- tokenization/tokenizer.py
+++ tokenization/tokenizer.py
@@ -1,5 +1,6 @@
+import os
+import unicodedata
 import regex as re
-import unicodedata, os
 
 # partern to split tokens
 GPT4LIKE_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
@@ -56,7 +57,6 @@ def render_token_to_file(t: bytes) -> str:
 
 class Tokenizer:
     def __init__(self) -> None:
-        self.pattern = re.compile(GPT4LIKE_SPLIT_PATTERN)
         self.merges = {}
         self.specials = {}
         self.vocab = {}
@@ -77,7 +77,7 @@ class Tokenizer:
         num_merges = vocab_size - 256
 
         # split the text up into chunks
-        text_chunks = re.findall(self.pattern, text)
+        text_chunks = re.findall(re.compile(GPT4LIKE_SPLIT_PATTERN), text)
 
         # vocab ids from training text
         text_vocab_ids = [list(txt_ch.encode("utf-8")) for txt_ch in text_chunks]
@@ -128,7 +128,8 @@ class Tokenizer:
                     vocab_ids = merge_pair(vocab_ids, pair, pair_id)
             return vocab_ids
 
-        text_chunks = re.findall(self.pattern, text)
+        text_chunks = re.findall(re.compile(GPT4LIKE_SPLIT_PATTERN), text)
+        print("pattern", text_chunks)
         ids = []
         for chunk in text_chunks:
             chunk_bytes = chunk.encode("utf-8")
@@ -144,26 +145,23 @@ class Tokenizer:
         model_file = model_name + ".model"
         with open(model_file, "w") as f:
             f.write("version 1\n")
-            f.write(f"{self.pattern}\n")
             f.write(f"{len(self.specials)}\n")
 
             for special, idx in self.specials.items():
                 f.write(f"{special} {idx}\n")
 
-            for idx1, idx2 in self.merges:
-                f.write(f"{idx1} {idx2}\n")
+            for (idx1, idx2), id in self.merges.items():
+                f.write(f"{idx1} {idx2} {id}\n")
 
     def load(self, model_file: str) -> None:
         assert model_file.endswith(".model")
 
         merges = {}
         specials = {}
-        idx = 256
 
         with open(model_file, "r", encoding="utf-8") as f:
             version = f.readline().strip()
             assert version == "version 1"
-            self.pattern = f.readline().strip()
             num_special = int(f.readline().strip())
             for _ in range(num_special):
                 special, special_idx = f.readline().strip().strip()
@@ -171,9 +169,8 @@ class Tokenizer:
 
             # read merges
             for line in f:
-                idx1, idx2 = map(int, line.split())
-                merges[idx1, idx2] = idx
-                idx += 1
+                idx1, idx2, id = map(int, line.split())
+                merges[idx1, idx2] = id
 
         self.merges = merges
         self.specials = specials
@@ -187,21 +184,23 @@ if __name__ == "__main__":
     file = "wikipedia_tartan.txt"
     with open(file, "r", encoding="utf-8") as f:
         content = f.read()
-        tokenizer.train(content, 500, True)
+        tokenizer.train(content, 5000, True)
 
     tokenizer.save("tok")
 
+    newtok = Tokenizer()
+
     filename = "tok.model"
     assert os.path.isfile(filename)
-    tokenizer.load(filename)
+    newtok.load(filename)
 
     print("----")
     file = "wikipedia_python.txt"
     with open(file, "r", encoding="utf-8") as f:
         content = f.read()
         text = content[:1000]
-        enc = tokenizer.encode(text)
-        dec = tokenizer.decode(enc)
+        enc = newtok.encode(text)
+        dec = newtok.decode(enc)
         print(text)
         print(enc)
         print(dec)
